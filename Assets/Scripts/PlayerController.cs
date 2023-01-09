7253d317fa2e8ba031d2d455ad86d4010abfbe27
Brolocuelo <luisalfonsorello@gmail.com>
2023-01-09 10:44:09 +0100

    Las mecánicas principales están hechas

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public float jumpForce = 10f;
+    private bool isOnTheGround = true;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnTheGround)
+        {
+            isOnTheGround = false;
+            _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
+        }
+    }
+
+    private void OnCollisionEnter(Collision otherCollider)
+    {
+        isOnTheGround = true;
+    }
+}
